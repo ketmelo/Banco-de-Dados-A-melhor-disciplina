268d0c1ad396acf6724b69ddefb2543a9e9206e0
Ketlyn De Melo <ketlynmelo002@gmail.com>
2023-09-11 20:18:41 -0300

    4 exercício

--- script_exemplos_ddl.sql
+++ script_exemplos_ddl.sql
@@ -42,3 +42,5 @@ CREATE TABLE matriculas (
 SELECT alunos.nome FROM  alunos
  INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
  WHERE matriculas.curso = 'Engenharia de Software';
+
+ SELECT produto, SUM(receita) AS receita_total FROM vendas GROUP BY produto;
