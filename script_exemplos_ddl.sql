5dbe584ce726d0441254a321a3925e881f7f08c2
Ketlyn De Melo <ketlynmelo002@gmail.com>
2023-09-12 20:49:39 -0300

    20 exercício

--- script_exemplos_ddl.sql
+++ script_exemplos_ddl.sql
@@ -104,3 +104,7 @@ SELECT alunos.nome, COUNT(matriculas.id) AS numero_de_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.nome;
+
+SELECT produto, COUNT(*) AS quantidade_transacoes
+FROM vendas
+GROUP BY produto;
