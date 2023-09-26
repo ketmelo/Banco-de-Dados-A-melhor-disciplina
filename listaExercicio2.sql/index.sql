f3ceee937bf8211ee7996b602977f439aea648aa
ketmelo <102187289+ketmelo@users.noreply.github.com>
2023-09-25 21:32:14 -0300

    ex2 lista2

--- listaExercicio2.sql/index.sql
+++ listaExercicio2.sql/index.sql
@@ -181,4 +181,14 @@ BEGIN
 END //
 DELIMITER ;
 
+CREATE PROCEDURE sp_LivrosPorCategoria(IN nomeCategoria VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Categoria.Nome = nomeCategoria;
+END //
+DELIMITER ;
 
