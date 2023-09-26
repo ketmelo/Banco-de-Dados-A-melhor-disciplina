8d605598df456aa1cb6fe44ffe632fd52d3b9a95
Ketlyn Melo <ketlynmelo002@gmail.com>
2023-09-25 22:54:27 -0300

    ex10 lista2

--- listaExercicio2.sql/listaDeExercicio02_DDL_e_DML.sql
+++ listaExercicio2.sql/listaDeExercicio02_DDL_e_DML.sql
@@ -272,3 +272,18 @@ BEGIN
 END //
 DELIMITER ;
 
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+
+
+END //
+
+DELIMITER ;
+
